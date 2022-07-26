fc6d108b0693100c34800ff5f6dadab9662f4a01
Shreshu <abdshubham13@gmail.com>
2022-07-26 22:37:34 +0530

    cros error

--- src/main/java/com/project/bookstore/BookService.java
+++ src/main/java/com/project/bookstore/BookService.java
@@ -24,16 +24,16 @@ public class BookService {
 	}
 	
 	public List<BookDto> getBooks(){
-	Iterable<Book> all=	bookRepository.findAll();
-	List<BookDto> actualList = StreamSupport
-			  .stream(all.spliterator(), false).map(book->modelMapper.map(book, BookDto.class))
-			  .collect(Collectors.toList());
+//	Iterable<Book> all=	bookRepository.findAll();
+//	List<BookDto> actualList = StreamSupport
+//			  .stream(all.spliterator(), false).map(book->modelMapper.map(book, BookDto.class))
+//			  .collect(Collectors.toList());
 	
-//	List<BookDto> actualList= new ArrayList<>();
-//	BookDto book = BookDto.methodBuilder().title("my first book title").build();
-//	BookDto book2 = BookDto.methodBuilder().title("my second book title").build();
-//	actualList.add(book);
-//	actualList.add(book2);
+	List<BookDto> actualList= new ArrayList<>();
+	BookDto book = BookDto.methodBuilder().title("my first book title").build();
+	BookDto book2 = BookDto.methodBuilder().title("my second book title").build();
+	actualList.add(book);
+	actualList.add(book2);
 	return actualList;
 
 	}
