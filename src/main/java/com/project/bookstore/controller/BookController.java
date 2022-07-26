04608c71ec653865c0860edd3d1256600fb4aba3
Shreshu <abdshubham13@gmail.com>
2022-07-26 12:21:28 +0530

    bookstore added second

--- src/main/java/com/project/bookstore/controller/BookController.java
+++ src/main/java/com/project/bookstore/controller/BookController.java
@@ -15,8 +15,10 @@ public class BookController {
 	@GetMapping("api/v1/books")
 	public ResponseEntity<List<BookDto>> getBooks(){
 		BookDto book = BookDto.methodBuilder().title("my first book title").build();
+		BookDto book2 = BookDto.methodBuilder().title("my second book title").build();
 	List<BookDto> books = new ArrayList<>();
 	books.add(book);
+	books.add(book2);
 	return ResponseEntity.ok(books);
 	}
 }
